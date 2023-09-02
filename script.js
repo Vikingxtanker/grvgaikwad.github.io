0687357ec8b693c164e1b35db5ca750d2d9d0b16
Vikingxtanker <60213882+Vikingxtanker@users.noreply.github.com>
2023-09-02 23:52:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+document.getElementById('symptomInput').addEventListener('input', function () {
+    const inputSymptom = this.value.toLowerCase();
+    const symptomSuggestions = document.getElementById('symptomSuggestions');
+    symptomSuggestions.innerHTML = "";
+
+    // Basic symptom data (for demonstration only)
+    const symptoms = [
+        "runny nose", "sneezing", "cough", "fever", "body aches",
+        "fatigue", "itchy eyes", "nasal congestion"
+        // Add more symptoms here
+    ];
+
+    // Find matching symptoms and display suggestions
+    const matchingSymptoms = symptoms.filter(symptom =>
+        symptom.toLowerCase().includes(inputSymptom)
+    );
+
+    matchingSymptoms.forEach(matchingSymptom => {
+        const suggestionItem = document.createElement('div');
+        suggestionItem.classList.add('suggestion-item');
+        suggestionItem.textContent = matchingSymptom;
+        suggestionItem.addEventListener('click', function () {
+            addSymptom(matchingSymptom);
+        });
+        symptomSuggestions.appendChild(suggestionItem);
+    });
+});
+
+function addSymptom(symptom) {
+    const symptomDropdown = document.getElementById('symptomDropdown');
+    const newOption = document.createElement('option');
+    newOption.value = symptom;
+    newOption.text = symptom;
+    symptomDropdown.appendChild(newOption);
+    document.getElementById('symptomInput').value = ""; // Clear the input
+    document.getElementById('symptomSuggestions').innerHTML = ""; // Clear suggestions
+}
+
 document.getElementById('checkButton').addEventListener('click', function () {
     const selectedSymptoms = Array.from(document.getElementById('symptomDropdown').selectedOptions).map(option => option.value.toLowerCase());
     let matchingDiseases = [];
