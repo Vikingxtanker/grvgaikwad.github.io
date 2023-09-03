a2f9918533394f3517f49f33e61369d269d1a85e
Vikingxtanker <60213882+Vikingxtanker@users.noreply.github.com>
2023-09-03 13:18:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
-document.getElementById('checkButton').addEventListener('click', function () {
-    const selectedSymptoms = Array.from(document.getElementById('symptomDropdown').selectedOptions).map(option => option.value);
+document.getElementById('symptomInput').addEventListener('input', function () {
+    const inputSymptom = this.value.toLowerCase();
+    const symptomSuggestions = document.getElementById('symptomSuggestions');
+    symptomSuggestions.innerHTML = "";
 
-    if (selectedSymptoms.length === 0) {
-        document.getElementById('diagnosisResult').textContent = "Please select at least one symptom.";
-        return;
+    // Basic symptom data (for demonstration only)
+    const symptoms = [
+        "runny nose", "sneezing", "cough", "fever", "body aches",
+        "fatigue", "itchy eyes", "nasal congestion"
+        // Add more symptoms here
+    ];
+
+    // Find matching symptoms and display suggestions
+    const matchingSymptoms = symptoms.filter(symptom =>
+        symptom.toLowerCase().includes(inputSymptom)
+    );
+
+    matchingSymptoms.forEach(matchingSymptom => {
+        const suggestionItem = document.createElement('div');
+        suggestionItem.classList.add('suggestion-item');
+        suggestionItem.textContent = matchingSymptom;
+        suggestionItem.addEventListener('click', function () {
+            addSymptom(matchingSymptom);
+        });
+        symptomSuggestions.appendChild(suggestionItem);
+    });
+});
+
+function addSymptom(symptom) {
+    const symptomDropdown = document.getElementById('symptomDropdown');
+    const newOption = document.createElement('option');
+    newOption.value = symptom;
+    newOption.text = symptom;
+    symptomDropdown.appendChild(newOption);
+    document.getElementById('symptomInput').value = ""; // Clear the input
+    document.getElementById('symptomSuggestions').innerHTML = ""; // Clear suggestions
+}
+
+document.getElementById('checkButton').addEventListener('click', function () {
+    const selectedSymptoms = Array.from(document.getElementById('symptomDropdown').selectedOptions).map(option => option.value.toLowerCase());
+    let matchingDiseases = [];
+
+    // Basic disease and symptom data (for demonstration only)
+    const diseases = [
+        { name: "Common Cold", symptoms: ["runny nose", "sneezing", "cough"] },
+        { name: "Flu", symptoms: ["fever", "body aches", "fatigue"] },
+        { name: "Allergies", symptoms: ["itchy eyes", "sneezing", "nasal congestion"] }
+        // Add more diseases and symptoms here
+    ];
+
+    // Iterate through diseases and check for matching symptoms
+    for (const disease of diseases) {
+        if (selectedSymptoms.every(symptom => disease.symptoms.includes(symptom))) {
+            matchingDiseases.push(disease.name);
+        }
     }
 
-    const symptomQuery = selectedSymptoms.join(',');
+    let diagnosisResult = "";
+    if (matchingDiseases.length > 0) {
+        diagnosisResult = `Diseases related to the selected symptoms: ${matchingDiseases.join(', ')}.`;
+    } else {
+        diagnosisResult = "No specific diseases found related to the selected symptoms.";
+    }
 
-    // Replace this with your API integration code
-    fetch(`YOUR_API_ENDPOINT?symptoms=${symptomQuery}&api_key=YOUR_API_KEY`)
-        .then(response => response.json())
-        .then(data => {
-            // Process the API response and update the diagnosis result and probability
-            // Example: const diagnosisResult = data.result;
-            // Example: const probability = data.probability;
-
-            // Update the HTML elements with the diagnosis and probability
-            // document.getElementById('diagnosisResult').textContent = diagnosisResult;
-            // document.getElementById('diagnosisProbability').textContent = `Probability: ${probability}%`;
-        })
-        .catch(error => {
-            console.error(error);
-            document.getElementById('diagnosisResult').textContent = "An error occurred while fetching diagnosis data.";
-        });
+    document.getElementById('diagnosisResult').textContent = diagnosisResult;
 });
